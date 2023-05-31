3153e654e88f1945ba15e588bc959ff7130f5267
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-05-31 11:51:08 +0300

    class -> hook for App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, useEffect, useState } from "react";
 import { nanoid } from 'nanoid';
 import { Notify } from 'notiflix';
 import AddContactsForm from "./AddContactsForm";
@@ -6,68 +6,117 @@ import Contacts from "./Contacts";
 import { StyledContainer } from './App.styled';
 import { getItemLocalStorage, setItemLocalStorage } from "localStorage";
 
-export class App extends Component {
-  state = {
-    contacts: [],
-  };
+export function App(){
+  const [contacts, setContacts] = useState([])
 
-  componentDidMount() {
-    if(!getItemLocalStorage()){
-      setItemLocalStorage(this.state.contacts)
-    } else {
-      this.setState({
-        contacts: JSON.parse(getItemLocalStorage())
-      })
-    }
-  };
-
-  componentDidUpdate(_, prevState) {
-    if(prevState.contacts.length !== this.state.contacts.length){
-      setItemLocalStorage(this.state.contacts)
-    }
-  }
-
-  createContact(data){
+  // useEffect(()=>{
+  //   if(!getItemLocalStorage()){
+  //     setItemLocalStorage(contacts)
+  //   } else {
+  //       setContacts(JSON.parse(getItemLocalStorage()))
+  //     }
+  //   }, []);
+  
+  function createContact(data){
     const newContact = {
       ...data,
       id: nanoid(),
     }
-    this.addContact(newContact)
+    addContact(newContact)
   };
 
-  addContact = (newContact) => {
-    if(this.state.contacts.some((item)=>{
+  function addContact(newContact) {
+    if(contacts.some((item)=>{
       return item.name.toLowerCase() === newContact.name.toLowerCase()
     })) {
       Notify.failure('Такий контакт вже записано!')
       return
     }
     Notify.success('Контакт записано!')
-    this.setState((prevState)=>{
-      return {
-        contacts: [...prevState.contacts, newContact]
-      }
-    })
-};
+    setContacts((prevState)=>[...prevState, newContact])
+  };
 
-  deleteContact = (contactId) => {
-    this.setState((prevState)=>({
-      contacts: prevState.contacts.filter( (item) => item.id !== contactId),
-    }))
+  function deleteContact(contactId) {
+    setContacts(contacts.filter( (item) => item.id !== contactId))
     Notify.info('Контакт видалено!')
   };
 
-  render() {
-    return (
-      <StyledContainer>
-        <AddContactsForm 
-          createContact={(data)=>this.createContact(data)}
-        />
-        <Contacts 
-          contacts={this.state.contacts}
-          onDeleteContact = {this.deleteContact}
-        />
-      </StyledContainer>
-    );
-  };
-};
+  return (
+    <StyledContainer>
+      <AddContactsForm 
+        createContact={(data)=>createContact(data)}
+      />
+      <Contacts 
+        contacts={contacts}
+        onDeleteContact = {deleteContact}
+      />
+    </StyledContainer>
+  );
+}
+
+
+// export class App extends Component {
+//   state = {
+//     contacts: [],
+//   };
+
+//   componentDidMount() {
+//     if(!getItemLocalStorage()){
+//       setItemLocalStorage(this.state.contacts)
+//     } else {
+//       this.setState({
+//         contacts: JSON.parse(getItemLocalStorage())
+//       })
+//     }
+//   };
+
+//   componentDidUpdate(_, prevState) {
+//     if(prevState.contacts.length !== this.state.contacts.length){
+//       setItemLocalStorage(this.state.contacts)
+//     }
+//   }
+
+//   createContact(data){
+//     const newContact = {
+//       ...data,
+//       id: nanoid(),
+//     }
+//     this.addContact(newContact)
+//   };
+
+//   addContact = (newContact) => {
+//     if(this.state.contacts.some((item)=>{
+//       return item.name.toLowerCase() === newContact.name.toLowerCase()
+//     })) {
+//       Notify.failure('Такий контакт вже записано!')
+//       return
+//     }
+//     Notify.success('Контакт записано!')
+//     this.setState((prevState)=>{
+//       return {
+//         contacts: [...prevState.contacts, newContact]
+//       }
+//     })
+// };
+
+//   deleteContact = (contactId) => {
+//     this.setState((prevState)=>({
+//       contacts: prevState.contacts.filter( (item) => item.id !== contactId),
+//     }))
+//     Notify.info('Контакт видалено!')
+//   };
+
+//   render() {
+//     return (
+//       <StyledContainer>
+//         <AddContactsForm 
+//           createContact={(data)=>this.createContact(data)}
+//         />
+//         <Contacts 
+//           contacts={this.state.contacts}
+//           onDeleteContact = {this.deleteContact}
+//         />
+//       </StyledContainer>
+//     );
+//   };
+// };

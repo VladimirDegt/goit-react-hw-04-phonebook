d5eb60c826b8827f380e1eb9f1b2cb69ae1a0970
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-05-31 17:51:00 +0300

    add eslink

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { nanoid } from 'nanoid';
 import { Notify } from 'notiflix';
 import AddContactsForm from "./AddContactsForm";

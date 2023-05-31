741eb578fa9523296caf85a030d74cb303d53be0
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-05-31 10:33:26 +0300

    class --> hook (contacts, addforms)

--- src/components/AddContactsForm/AddContactsForm.jsx
+++ src/components/AddContactsForm/AddContactsForm.jsx
@@ -1,66 +1,70 @@
 import { nanoid } from 'nanoid'
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { StyledSection, StyledForm, StyledButton } from './AddContactsForm.styled';
 
 const inputNameId = nanoid();
 const inputNumberId = nanoid();
 
-class AddContactsForm extends Component{
-  state = {
-    name: '',
-    number: '',
-  };
+function AddContactsForm({createContact}){
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleInputChange = ({target}) => {
-    this.setState({
-      [target.name]: target.value,
-    })
-  };
-
-  handleFormSubmit = (e) => {
-    e.preventDefault()
-    this.props.createContact({
-      name: this.state.name,
-      number: this.state.number,
-    })
-    this.setState({
-      name: '',
-      number: '',
-    }) 
-  };
-
-  render() {
-    return <StyledSection>
-            <h1>Phonebook</h1>
-            <StyledForm onSubmit={this.handleFormSubmit}>
-              <label htmlFor={inputNameId}>Name</label> 
-              <input
-                id={inputNameId}
-                type="text"
-                name="name"
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                required
-                onChange={this.handleInputChange}
-                value={this.state.name}
-              />
-              <label htmlFor={inputNumberId}>Number</label> 
-              <input
-                id={inputNumberId}
-                type="tel"
-                name="number"
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                required
-                onChange={this.handleInputChange}
-                value={this.state.number}
-              />
-              <StyledButton type="submit">Add contacts</StyledButton>
-            </StyledForm> 
-          </StyledSection>
+  function handleInputChange({target}) {
+    switch(target.name) {
+      case 'name':
+        setName(target.value)
+        break;
+      case 'number':
+        setNumber(target.value)
+        break;
+      default:
+        return;
     }
-};
+  };
+
+  function handleFormSubmit(e){
+    e.preventDefault()
+
+    createContact({
+      name,
+      number,
+    });
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <StyledSection>
+      <h1>Phonebook</h1>
+      <StyledForm onSubmit={handleFormSubmit}>
+        <label htmlFor={inputNameId}>Name</label> 
+        <input
+          id={inputNameId}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={handleInputChange}
+          value={name}
+        />
+        <label htmlFor={inputNumberId}>Number</label> 
+        <input
+          id={inputNumberId}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={handleInputChange}
+          value={number}
+        />
+        <StyledButton type="submit">Add contacts</StyledButton>
+      </StyledForm> 
+    </StyledSection>
+  )
+}
 
 AddContactsForm.propTypes = {
   createContact: PropTypes.func.isRequired,
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,42 +1,37 @@
-import { Component } from "react";
+import { useState } from "react";
 import PropTypes from 'prop-types';
 import { StyledSection } from './Contacts.styled';
 import Filter from "components/Filter/Filter";
 import ContactList from "components/ContactList/ContactList";
 
-class Contacts extends Component {
+function Contacts({contacts, onDeleteContact}){
+  const [vidibleContacts, setVidibleContacts] = useState('');
 
-  state = {
-    vidibleContacts: '',
+  function handleInputChange({target}){
+    setVidibleContacts(target.value)
   }
 
-  handleInputChange = ({target}) => {
-    this.setState({
-      [target.name]: target.value
-    })
-  }
-
-  findContact = () => {
-    return this.props.contacts.filter((item) => 
-      item.name.toLowerCase().includes(this.state.vidibleContacts.toLowerCase())
+  function findContact(){
+    return contacts.filter((item) => 
+      item.name.toLowerCase().includes(vidibleContacts.toLowerCase())
     )
   }
 
-  render() {
-    return  <StyledSection>
-              <h2>Contacts</h2>
-              <Filter 
-                handleInputChange = {this.handleInputChange}
-                value={this.state.vidibleContacts}
-              />
-              <ContactList
-                findValue = {this.state.vidibleContacts}
-                findContact = {this.findContact}
-                onDeleteContact = {this.props.onDeleteContact}
-              />
-            </StyledSection>
-  }  
-};
+  return (
+    <StyledSection>
+      <h2>Contacts</h2>
+      <Filter 
+        handleInputChange = {handleInputChange}
+        value={vidibleContacts}
+      />
+      <ContactList
+        findValue = {vidibleContacts}
+        findContact = {findContact}
+        onDeleteContact = {onDeleteContact}
+      />
+    </StyledSection>
+  )
+}
 
 Contacts.propTypes = {
   contacts: PropTypes.arrayOf(
